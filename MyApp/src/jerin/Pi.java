c849ad11137683c0d0a3430514c24d7c2e32bfe1
Jerin <jerinjoy007@gmail.com>
2021-05-03 14:28:46 +1000

    inserting results to the database and displaying it on the admin page.

--- MyApp/src/jerin/Pi.java
+++ MyApp/src/jerin/Pi.java
@@ -8,10 +8,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
+
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 public class Pi extends HttpServlet {
 	private static Connection connection;
@@ -21,7 +25,7 @@ public class Pi extends HttpServlet {
 	public static void connect() throws SQLException {
 
 		System.out.println("Connecting..");
-		String url = "jdbc:mysql://localhost:3306/mymusic";
+		String url = "jdbc:mysql://localhost:3306/nrspp";
 		String user = "root";
 		String password = "root";
 		try {
@@ -48,63 +52,50 @@ public class Pi extends HttpServlet {
 	} // disconnect
 
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		String sector = req.getParameter("sector");
-		String currency = req.getParameter("currency");
-		String revE = req.getParameter("revE");
-		String name = null;
-		int annualSales = Integer.parseInt(req.getParameter("1.3"));
-		int insCost = Integer.parseInt(req.getParameter("2.1"));
-		int incidents = Integer.parseInt(req.getParameter("2.2"));
-//		try {
-////			connect();
-////			String queryString = "select * from music_album where id =?";
-////			preparedStatement = connection.prepareStatement(queryString);
-////			preparedStatement.setString(1, "a20");
-////	        ResultSet rs = preparedStatement.executeQuery();
-////	        if(rs.next())
-////	    	System.out.println(name=rs.getString(2)); 
-////	        disconnect();
-//	            
-//	        
-////		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-////			e.printStackTrace();
-//		}
 		
-		int vehicles = Integer.parseInt(req.getParameter("2.3"));
-		int distance = Integer.parseInt(req.getParameter("2.4"));
-		int fleetInsPreAnnual = Integer.parseInt(req.getParameter("2.5"));
-		int premiumPerVe = fleetInsPreAnnual/vehicles;
-		int excessPC = Integer.parseInt(req.getParameter("2.7"));
-
-//		int i = Integer.parseInt(req.getParameter("annualSales"));
-
-		PrintWriter out = res.getWriter();
-		out.println("<HTML>\n" + "<HEAD> </HEAD>\n" + "<BODY>\n" + "<H2> NRSPP Calculator </H2>\n" +
-
-				"<p> Hi, this is the first version of National Road Safety Partnership "
-				+ " Program accident cost calculator. The style sheet is pretty basic and"
-				+ " a final version will be added later.		        </p>" + "The sector you are in " + sector
-				+ " </br>" + "Your chosen currency is " + currency + "</br>" + "And your main revenue earner is " + revE +" "+name
-				+ "</br>" 
-//				"The annual sale you entered "+annualSales + "</br>" +
-//				"The total insurers cost is "+ insCost+ "</br>" +
-//				"The total incidents "+ incidents+ "</br>" +
-//				"The total vehicles "+ vehicles + "</br>" +
-//				"The total distance covered  "+ distance+
+		
+		String sector = req.getParameter("sector");
+		int profit = Integer.parseInt(req.getParameter("2.10"));
+		int directCost = Integer.parseInt(req.getParameter("3.4"));
+		int indirectC = Integer.parseInt(req.getParameter("3.7"));
+		int mfCost = Integer.parseInt(req.getParameter("3.10"));
+		int avCost = Integer.parseInt(req.getParameter("3.13"));
 
 
-				);
-		out.println("\nThe sector you are in "+sector +" </br>");
-		out.println("\nAnd your main revenue earner is "+revE);
-		out.println("\nThe annual sale you entered "+annualSales);
-		out.println("\nThe total insurers cost is "+ insCost);
-		out.println("\nThe total incidents "+ incidents);
-		out.println("\nThe total vehicles "+ vehicles);
-		out.println("\nThe total distance covered  "+ distance);
-		out.println("\nThe Fleets Annual insurance premium  "+ fleetInsPreAnnual);
-		out.println("\nThe premium per vehicle "+ premiumPerVe);
-		out.println("\nThe excess amount per claim "+ excessPC+"</BODY>\n" + "</HTML>");
+		try {
+			connect();
+			System.out.println(sector+profit+" "+directCost+" "+indirectC+" "+mfCost);
+			String query = "insert into report values ('"+sector+"', "+profit+", "+directCost+", "+indirectC+", "
+					+mfCost+", "+avCost+")"; 
+			int i =statement.executeUpdate(query);
+			if(i>0)
+			{
+				PrintWriter out = res.getWriter();
+				out.println("<HTML>\n" + "<HEAD> </HEAD>\n" + "<BODY>\n" + "<H2> NRSPP Calculator </H2>\n" +
+
+						"<p> Hi, this is the first version of National Road Safety Partnership "
+						+ " Program accident cost calculator. The style sheet is pretty basic and"
+						+ " a final version will be added later.		        </p>" + "The sector you are in " + sector
+						+ "</br>" 
+		);
+				
+				
+				out.println("<p> <B> Succesfully inserted the values into the database for future reference</B></p> </BODY>\n" + "</HTML>");
+			}
+			else
+			{
+				PrintWriter out = res.getWriter();
+				out.println("<HTML>\n" + "<HEAD> </HEAD>\n" + "<BODY>\n" + "<H2> NRSPP Calculator </H2>\n" );
+				out.println("<H2><B> An Error occured while trying to save your result to the database </B></H2> </BODY>\n" + "</HTML>");
+			}
+			
+					
+		} catch (SQLException e) 
+		{
+			e.getMessage();
+			System.out.println(e.getSQLState()+" \n"+e.getMessage());
+		}
+		
 		
 		 
 		System.out.println("We got the number you entered " + "annualSales");
--- MyApp/src/jerin/WelcomeServlet.java
+++ MyApp/src/jerin/WelcomeServlet.java
@@ -2,26 +2,127 @@ package jerin;
 
 import java.io.IOException;  
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;  
 import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpServletResponse;
+
+
   
 public class WelcomeServlet extends HttpServlet {  
+	
+	private static Connection connection;
+	private static Statement statement;
+	private static PreparedStatement preparedStatement;
+
+	public static void connect() throws SQLException {
+
+		System.out.println("Connecting..");
+		String url = "jdbc:mysql://localhost:3306/nrspp";
+		String user = "root";
+		String password = "root";
+		try {
+			// Register JDBC driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Trying to register..");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Trying to connect..");
+
+		connection = DriverManager.getConnection(url, user, password);
+		statement = connection.createStatement();
+	} // connect
+
+	public static void disconnect() throws SQLException {
+		if (preparedStatement != null)
+			preparedStatement.close();
+		if (statement != null)
+			statement.close();
+		if (connection != null)
+			connection.close();
+		System.out.println("Disconnected");
+	} 
+	
 public void doPost(HttpServletRequest request, HttpServletResponse response)  
     throws ServletException, IOException {  
   
     response.setContentType("text/html");  
     PrintWriter out = response.getWriter(); 
+    try {
+		connect();
+		String queryString = "select * from report";
+//		preparedStatement = connection.prepareStatement(queryString);
+//		preparedStatement.setString(1, "a20");
+		statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(queryString);
+        if (rs != null) {
+            // There's a ResultSet to be had
+            ResultSet rs1 = statement.getResultSet();
+            out.println("<html>\r\n" + 
+            		"  <head>\r\n" + 
+            		"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n" + 
+//            		"    <link rel=\"stylesheet\" href=\"cal.css\" />\r\n" + 
+            		"    <title>Welcome Admin</title>\r\n" + 
+            		"  </head>"
+            		+ "<BODY>");
+            out.append("<P ALIGN='center'><TABLE BORDER=”1”>");
+            out.append("<TABLE cellspacing=\"3\" bgcolor=\"#000000\">\n");
+            out.append("<caption><B>Incident Cost Calculator Reuslts</B></caption>");
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            int numcols = rsmd.getColumnCount();
+        
+            // Title the table with the result set's column labels
+            out.append("<TR bgcolor=\"#ffffff\">");
+            for (int i = 1; i <= numcols; i++)
+              out.append("<TH>" + rsmd.getColumnLabel(i));
+            out.append("</TR>\n");
+
+            while(rs.next()) {
+              out.append("<TR bgcolor=\"#ffffff\">");  // start a new row
+              for(int i = 1; i <= numcols; i++) {
+                out.append("<TD>");  // start a new data element
+                Object obj = rs.getObject(i);
+                if (obj != null)
+                  out.append(obj.toString());
+                else
+                  out.append("&nbsp;");
+                }
+              out.append("</TR>\n");
+            }
+
+            // End the table
+            out.append("</TABLE>\n");
+          }
+          else {
+            // There's a count to be had
+            out.append("<B>Records Affected:</B> " + statement.getUpdateCount());
+          }
+        if(rs.next())
+        System.out.println(rs.getString(2));
+        disconnect();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
     
     
           
     String n=request.getParameter("username");  
-    out.print("Welcome Admin"+n);  
-	out.println("<HTML>\n" + "<HEAD> </HEAD>\n" + "<BODY>\n" + "<H2> NRSPP Calculator </H2>\n" +
-
+   
+	out.println(
 			"<p> Hi, this is the first version of National Road Safety Partnership "+
 			"<h> We inted to display previous results saved in the database "+
 
